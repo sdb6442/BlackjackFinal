ce771cad4fab292706c81aae7750fffac29dfbc4
Tristan Hall <Tristan74Hall@gmail.com>
2023-11-27 19:08:44 -0500

    add html compatibility

--- blackjack.go
+++ blackjack.go
@@ -5,11 +5,52 @@
 package main
 
 import (
+	"embed"
 	"fmt"
+	"html/template"
+	"io"
+	"io/fs"
 	"math/rand"
 	"reflect"
 	"time"
 )
+
+const (
+	layoutsDir   = "templates/layouts"
+	templatesDir = "templates"
+	extension    = "/*.html"
+	gamepage = "home.html"
+)
+
+var (
+	//go:embed templates/* templates/layouts/*
+	files     embed.FS
+	templates map[string]*template.Template
+)
+
+func LoadTemplates() error {
+	if templates == nil {
+		templates = make(map[string]*template.Template)
+	}
+	tmplFiles, err := fs.ReadDir(files, templatesDir)
+	if err != nil {
+		return err
+	}
+
+	for _, tmpl := range tmplFiles {
+		if tmpl.IsDir() {
+			continue
+		}
+
+		pt, err := template.ParseFS(files, templatesDir+"/"+tmpl.Name(), layoutsDir+extension)
+		if err != nil {
+			return err
+		}
+
+		templates[tmpl.Name()] = pt
+	}
+	return nil
+}
 
 type errorString struct {
 	s string
